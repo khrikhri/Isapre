677bb1794a2c8872900a9c72c540a1c5b9717f6d
khrikhri <christian.zaballa@usach.cl>
2017-07-16 14:14:26 -0400

    asdd

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,5 @@
 require_relative 'boot'
+require 'csv'
 
 require 'rails/all'
 
@@ -11,5 +12,10 @@ module Prueba
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'America/Santiago'
+ 		config.active_record.default_timezone = :local # Or :utc
+    config.i18n.enforce_available_locales = true
+	  config.i18n.available_locales = [:es]
+	  config.i18n.default_locale = :es
   end
 end
